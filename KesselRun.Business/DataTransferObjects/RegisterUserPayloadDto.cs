c23b08d6fc2562db83d21deb2e3ec7e67f576fee
David Rogers <davidrogersau@yahoo.com.au>
2020-04-07 17:34:16 +1000

    Added Payload validator. This is different to the validator which is invoked by the PipelineBehavior. This validates the payload sent to the server; the POST. The other validator gets invoked after that, so long as validation on this one passes.

--- KesselRun.Business/DataTransferObjects/RegisterUserPayloadDto.cs
+++ KesselRun.Business/DataTransferObjects/RegisterUserPayloadDto.cs
@@ -3,5 +3,7 @@
     public class RegisterUserPayloadDto
     {
         public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
---
+++ KesselRun.Business/Validation/RegisterUserPayloadDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using KesselRun.Business.DataTransferObjects;
+
+namespace KesselRun.Business.Validation
+{
+    public class RegisterUserPayloadDtoValidator : AbstractValidator<RegisterUserPayloadDto>
+    {
+        public RegisterUserPayloadDtoValidator()
+        {
+            RuleFor(p => p.Password).NotEmpty(); // defer password strength validation to Identity library
+            RuleFor(p => p.ConfirmPassword)
+                .NotEmpty()
+                .Equal(p => p.Password);
+            RuleFor(p => p.UserName).NotEmpty();
+        }
+    }
+}
